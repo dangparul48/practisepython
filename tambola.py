5cd5b75f1a1b8dc3064d8efb4e5b925ea583c402
dangparul48 <40570163+dangparul48@users.noreply.github.com>
2020-06-23 12:50:10 +0530

    Matrix printing done

--- tambola.py
+++ tambola.py
@@ -16,7 +16,7 @@ def wordify(N):
     tens_dig = int(N / 10)
     num_in_words = ""
     if tens_dig is 0:
-        num_in_words = "Only number "
+        num_in_words = "Only number"
     else:
         num_in_words = words[tens_dig] + " " + words[ones_dig]
     return num_in_words
@@ -26,6 +26,15 @@ def generate_random():
     return random.randrange(1, 100)
 
 
+def print_opened_chart(numbers):
+    for i in range(1, 91):
+        if i in numbers:
+            print(i, end='\t')
+        else:
+            print(".", end='\t')
+        if i % 10 == 0:
+            print()
+
 # Main program begins from here
 numbers = []
 eng = inflect.engine()  # inflect module is used to convert the number into words
@@ -34,7 +43,7 @@ speakeng = pyttsx3.init()   # pytssx3 engine
 print("Press Enter to open next number")
 print("Press 1 to get the opened numbers")
 print("Press 2 to exit")
-while len(numbers) <= 99:
+while len(numbers) < 90:
     user_choice = input()
     if user_choice is '':
         num = generate_random()
@@ -45,7 +54,7 @@ while len(numbers) <= 99:
         speakeng.say(wordify(num) + " " + eng.number_to_words(num))
         speakeng.runAndWait()
     elif user_choice is '1':
-        print(numbers)
+        print_opened_chart(numbers)
     elif user_choice is '2':
         exit()
     else:
